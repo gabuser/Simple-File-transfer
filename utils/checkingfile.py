b9e478e735a199569c4817ce7bf57d27b5a71e97
gabuser <trabalhoinvest100@gmail.com>
2025-08-09 23:35:18 -0300

    binarysearch added

---
+++ utils/checkingfile.py
@@ -0,0 +1,10 @@
+import binarysearch
+from os import path, getcwd
+
+"""paths = getcwd()
+exists = binarysearch.searchfile('ut',paths)
+
+print(path.isfile(exists))"""
+
+def checking(paths):
+    pass
