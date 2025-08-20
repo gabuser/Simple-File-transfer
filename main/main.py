48659209bdf333345172cd984a486c9c1b3f02ef
gabuser <trabalhoinvest100@gmail.com>
2025-08-20 20:18:57 -0300

    manage listfilequeue

--- main/main.py
+++ main/main.py
@@ -14,21 +14,33 @@ listsfiles = listdir(directory)
 current_path = getcwd()
 corroutines:list
 readingqueue = asyncio.Queue()
+chekingqueue = asyncio.Queue()
+listqueue = asyncio.Queue()
 
 class server:
-    async def block(self,lists:list,dire:str):
+    async def block_search(self,lists:list):
         self.data = await asyncio.to_thread(binarysearch.searchfile,self.inputed, lists)
 
-        self.value = await asyncio.to_thread(checkingfile.checking,dire,self.data)
-
-    async def reading(self,onefile:str|list):
+    async def block_cheking(self,values:str|list, dire:str):
         
+        for value in values:
+            await listqueue.put(value)
+        
+        for _ in range(len(values)):
+            self.value = await asyncio.to_thread(checkingfile.checking,dire,await listqueue.get())
+            
+    async def reading(self,onefile:str|list):
+            
         match(onefile):
             case onefile if(type(onefile)) is str:
                 async with aiofiles.open(onefile,"rb") as opening:
                     readingmode = await opening.read()
                     await readingqueue.put((onefile,readingmode))
                     print(readingqueue)
+            
+            case onefile if(type(onefile)) is list:
+                #print(True)
+                pass
 
     async def main(self):
         choice= input("1: select one file each \n 2: choose all file \n 3: q to quit:")
@@ -37,13 +49,15 @@ class server:
             case '1':
                 while True:
                     self.inputed = input("insert a file:")
-                    await asyncio.gather(self.block(listsfiles,current_path))
+                    await asyncio.gather(self.block_search(listsfiles))
+                    await asyncio.gather(self.block_cheking(self.data, current_path))
                     await asyncio.gather(self.reading(self.data))
                     #await self.block(listsfiles,current_path)
                     #await self.reading(self.data)
         
             case '2':
-                pass
+                await asyncio.gather(self.block_cheking(listsfiles, current_path))
+                await asyncio.gather(self.reading(listsfiles))
 
 running= server()
 
